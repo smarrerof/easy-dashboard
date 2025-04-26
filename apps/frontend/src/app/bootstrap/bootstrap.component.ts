511014f58c2d4480acc26e0e097d9ecc6486cc0a
Sergio Marrero <smarrerof@gmail.com>
2025-04-26 20:58:27 +0100

    Cleanup

--- apps/frontend/src/app/bootstrap/bootstrap.component.ts
+++ apps/frontend/src/app/bootstrap/bootstrap.component.ts
@@ -4,9 +4,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: './bootstrap.component.html',
 })
-export class AppComponent {
+export class BootstrapComponent {
   title = 'frontend';
 }
